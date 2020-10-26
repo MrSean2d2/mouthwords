09c50a44fedd1b00744eaf9b73b273ba9f2f1e65
Sean Reitsma <sean@doubleflatwhite.nz>
2020-10-26 16:41:54 +1300

    Start implementing write to json

--- project.py
+++ project.py
@@ -1,9 +1,26 @@
 #! /usr/bin/env python3
 
+# project.py (untitled) - a script to make famous people say what you want
+# Copyright © 2020 Sean Reitsma
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # TODO Implement writing and reading from JSON files to avoid having to run the lengthy 
 # speech recognition process more often than necessary
 
 # TODO Add a check for framerate problems or somehow support differing framerates in videos
+
+# TODO Fix list ordering to allow for duplicate words
 
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import sys
@@ -15,25 +32,25 @@ import argparse
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-w", "--writejson", help="Write transcript to a JSON file", action="store_true")
+parser.add_argument("-r", "--readjson", help="Read from JSON transcript", action="store_true")
 parser.add_argument("wordfile", help="Text file of words to search for")
-parser.add_argument("files", nargs="+", help="MPEG4 files to search through - THESE MUST BE THE SAME FRAMERATE!")
+parser.add_argument("files", nargs="+", help="MPEG4 files (all with the same fps!) or JSON transcripts to search through")
 args = parser.parse_args()
 words = []
 words_immutable = []
 total_found_words = []
-if args.wordfile:
-        with open(args.wordfile, "r") as wordfilestring:
-            for line in wordfilestring:
-                for lyric in line.split():
-                    words.append(lyric)
-                    words_immutable.append(lyric)
-elif args.word:
-    words.append(args.word)
-    words_immutable.append(args.word)
+with open(args.wordfile, "r") as wordfilestring:
+    for line in wordfilestring:
+        for lyric in line.split():
+            words.append(lyric)
+            words_immutable.append(lyric)
 
-def cut_and_paste(dataList):
+
+
+def cut_and_paste(datalist):
     clips = []
-    for data in dataList:
+    for data in datalist:
         start = data["start"] - 0.01
         end = data["end"] + 0.01
         clip = VideoFileClip(data["file"]).subclip(start, end)
@@ -47,9 +64,9 @@ def cut_and_paste(dataList):
         
 
 
-def search(dataList, speechFile):
+def search(datalist, speechFile):
     word_data = []
-    for data in dataList:
+    for data in datalist:
         if "result" in data:
             for i in data["result"]:
                 for word in words:
@@ -92,16 +109,26 @@ def speech_recog_and_search(fileIn):
         if len(data) == 0:
             break
         if rec.AcceptWaveform(data):
-            datalist.append(json.loads(rec.Result()))
+            result = rec.Result()
+            datalist.append(json.loads(result))
 
-    datalist.append(json.loads(rec.FinalResult()))
+    finalResult = rec.FinalResult()
+    datalist.append(json.loads(finalResult))
+    if args.writejson:
+        with open("test.json", "w") as outputJson:
+            outputJson.write(json.dumps(datalist))
     found_words = search(datalist, fileIn)
     return found_words
 
 
 for file in args.files:
     print(f"Searching for words in {file}\nThis may take a while, please be patient...")
-    found_words = speech_recog_and_search(file)
+    if args.readjson:
+        with open(file, "r") as inputJson:
+            found_words = search(json.loads(inputJson.read()), file)
+
+    else:
+        found_words = speech_recog_and_search(file)
     total_found_words.extend(found_words)
 
 
